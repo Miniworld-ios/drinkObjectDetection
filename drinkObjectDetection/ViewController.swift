3d0afd2fcdb9a934ab810b2b53b9b76df5e24b9a
JminSim <jeong0983@naver.com>
2023-10-29 20:15:11 +0900

    Initial Commit

---
+++ drinkObjectDetection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  drinkObjectDetection
+//
+//  Created by 심정민 on 2023/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
